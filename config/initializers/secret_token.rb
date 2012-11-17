2cfbb753c85a4fc505ce6347f29569108b546895
Janice Leung <jjw1707@gmail.com>
2012-11-17 15:42:01 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Poodle::Application.config.secret_token = 'e1701fe6327802ec5f6fcdfac36744493d0eb7f672b6c95ecf7d0b3e038826fa7293bcd7811c035d35af27d42ac6c3d308fa3129791144a9776ec83a5cef6ae4'
