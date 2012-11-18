b7f6eaf4f155eab5b2784f7aec2ba995bce78d2c
Janice Leung <jjw1707@gmail.com>
2012-11-18 14:35:07 -0800

    events

--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,7 @@ Poodle::Application.routes.draw do
 
 
 
-  get "welcome/index"
+  get "welcome/index"   
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
