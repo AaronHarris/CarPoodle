fe81aab4a19b2e38555ffe2891365a6715f83ceb
Janice Leung <jjw1707@gmail.com>
2012-11-18 14:01:00 -0800

    got new event page to work

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -24,14 +24,14 @@ class EventsController < ApplicationController
   # GET /events/new
   # GET /events/new.json
   def new
-    # @event = Event.new
+    @event = Event.new
 
     respond_to do |format|
-      format.html new.html.erb
+      format.html # new.html.erb
       format.json { render json: @event }
     end
 
-    render :action => 'new'
+    # render :action => 'new'
   end
 
   # GET /events/1/edit
