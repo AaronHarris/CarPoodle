d0a11f337fb816685d6191e3fee4d0a845788c37
Douglass Chen <douglasschen@gmail.com>
2013-01-27 11:36:44 -0800

    Customized 404 page and testing to make sure this works

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery
+	protect_from_forgery
+
+	unless  Rails.application.config.consider_all_requests_local
+	  rescue_from Exception, :with => :render_404
+	end
+
+	private
+
+	def render_404
+		render :template => 'public/404', :layout => false, :status => :not_found
+		# raise ActionController::RoutingError.new('Not Found')
+	end
 end
