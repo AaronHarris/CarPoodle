1b0a74273f51b277e9cb63f10891717f6891f173
Ruyan Chen <fading.jade@gmail.com>
2013-03-08 16:17:23 -0800

    debugging 10

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
 	protect_from_forgery
-=begin
+
 	unless  Rails.application.config.consider_all_requests_local
 	  rescue_from Exception, :with => :render_404
 	end
@@ -11,5 +11,4 @@ class ApplicationController < ActionController::Base
 		render :template => 'public/404', :layout => false, :status => :not_found
 		# raise ActionController::RoutingError.new('Not Found')
 	end
-=end
 end
