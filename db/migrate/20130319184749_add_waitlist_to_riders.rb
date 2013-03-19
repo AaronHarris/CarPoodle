cb469bb8174c0f8c86d806dc69c3d64069142221
jjw1707 <jjw1707@gmail.com>
2013-03-19 13:10:21 -0700

    fixed waitlist migration problem

--- db/migrate/20130319184749_add_waitlist_to_riders.rb
+++ db/migrate/20130319184749_add_waitlist_to_riders.rb
@@ -2,13 +2,13 @@ class AddWaitlistToRiders < ActiveRecord::Migration
   def change
     add_column :riders, :waitlist, :boolean
 
-    Rider.reset_column_information
-    Rider.all.each do |r|
-    	if r.blank? then
-    		r.update(:waitlist => 1)
-    	else
-    		r.update(:waitlist => 0)
-    	end
-    end
+    # Rider.reset_column_information
+    # Rider.all.each do |r|
+    # 	if r.blank? then
+    # 		r.update(:waitlist => 1)
+    # 	else
+    # 		r.update(:waitlist => 0)
+    # 	end
+    # end
   end
 end
