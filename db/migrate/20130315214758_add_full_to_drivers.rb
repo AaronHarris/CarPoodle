045e2263915eb2ca44f92a2eb15b326feef1e36a
jjw1707 <jjw1707@gmail.com>
2013-03-23 15:40:22 -0700

    migration error?

--- db/migrate/20130315214758_add_full_to_drivers.rb
+++ db/migrate/20130315214758_add_full_to_drivers.rb
@@ -1,13 +1,13 @@
 class AddFullToDrivers < ActiveRecord::Migration
   def change
-    add_column :drivers, :full, :boolean, :default => 0
+    add_column :drivers, :full, :boolean, :default => false
 
     Driver.reset_column_information
     Driver.all.each do |d|
         if (d.spots > d.riders.size)
-            d.update(:full => 1)
+            d.update(:full => true)
         else 
-            d.update(:full => 0)
+            d.update(:full => false)
         end
     end
   end
